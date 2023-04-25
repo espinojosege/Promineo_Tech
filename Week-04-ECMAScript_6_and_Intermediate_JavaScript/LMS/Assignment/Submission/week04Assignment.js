865701202230b2ea999c73c48e33626313f68f77
espinojosege <104717830+espinojosege@users.noreply.github.com>
2023-04-25 15:52:43 -0700

    Update week04Assignment.js
    Edited for comments from iphone.

--- Week-04-ECMAScript_6_and_Intermediate_JavaScript/LMS/Assignment/Submission/week04Assignment.js
+++ Week-04-ECMAScript_6_and_Intermediate_JavaScript/LMS/Assignment/Submission/week04Assignment.js
@@ -3,7 +3,7 @@ function cLogRed(str) {                             // created function called '
 };                                                  // 1st parameter: '\x1b[31m' is the color red, '%s' is a placeholder for the string, '\x1b[0m' resets the color on exit
                                                     // 2nd parameter: the string which will be placed inside the first parameter. will replace '%s' when code logs to console.
                                                     // can also be written as followed: console.log('\x1b[31mString\x1b[0m')
-let ages = [3, 9, 23, 64, 2, 8, 28, 93];
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];            // create array called ages with 8 objects.
 console.log(`\n\n! ~~~~~     Week 04 Assignment      ~~~~~ !`)
 console.log(`\n\n! ~~~~~        Question 1           ~~~~~ !`)
 cLogRed(`\n\n1.  Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.`);
@@ -13,6 +13,10 @@ cLogRed(`1a.Programmatically subtract the value of the first element in the arra
 Do not use numbers to reference the last element, find it programmatically.
 ages[7] – ages[0] is not allowed!`);
 console.log(`(ages[ages.length - 1]) - ages[0] = ${ages[ages.length - 1] - ages[0]}\n`);
+// created a subtraction problem that subtracts the first element in an array from the last element in the array.
+// array[array.length-1] = last item in the array
+// array[0] = first element of the array.
+// 
 
 cLogRed(`1b. Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).`);
 ages.push(1996);
