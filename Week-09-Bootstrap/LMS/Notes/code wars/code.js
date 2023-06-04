5b32d6148a7fe1f4fcdd41cb4ae20c0d8615f9ab
espinojosege <EspinojoseG@gmail.com>
2023-06-03 23:15:07 -0700

    finished week09 Coding assignment

---
+++ Week-09-Bootstrap/LMS/Notes/code wars/code.js
@@ -0,0 +1,17 @@
+/*function greet (name, owner) {
+    //if else to compare
+    let msg
+    if (name === owner) {
+        msg = "Hello boss"
+    } else {
+        msg = "Hello guest"
+    }
+
+    return msg;
+}
+
+console.log(greet("jose", "notJose"))*/
+
+function stringToArray(string) {
+    split()
+}
